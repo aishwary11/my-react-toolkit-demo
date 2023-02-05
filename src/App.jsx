cb5959d69ea6db9ca73660354b5dbef80e882fe0
aishwary11 <aishwary46@gmail.com>
2023-02-05 15:53:17 +0530

    selector

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { Abouts } from "./components/About";
 import { postInfo } from "./slices/postSlice";
 import { userInfo } from "./slices/userSlice";
 
@@ -20,6 +21,7 @@ export default function App() {
       {userSelect.map((e, i) => <p key={i}>{e.id} {e.name}</p>)}
       <p>Post</p>
       {postSelect.map((e, i) => <p key={i}>{e.id} {e.title}</p>)}
+      <Abouts />
     </div>
   );
 }
